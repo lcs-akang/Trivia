4e7460f84b73eadfaf23b027bad1303023455d63
Aidan Kang <akang@lcs.on.ca>
2023-04-20 09:25:11 -0400

    Added a network service helper to load a joke from an endpoint, and displayed it in the user interface.

--- Trivia/Views/TriviaView.swift
+++ Trivia/Views/TriviaView.swift
@@ -13,35 +13,44 @@ struct TriviaView: View {
     
     @State var answerOpacity = 0.0
     
-    @State var currentTrivia = exampleTrivia
+    @State var currentTrivia: Trivia?
     
     // MARK: Computed properties
     var body: some View {
         NavigationView {
             VStack {
-                Text(currentTrivia.question)
-                    .font(.title)
-                    .multilineTextAlignment(.center)
                 
-                Button(action: {
-                    withAnimation(.easeIn(duration: 1.0)) {
-                        answerOpacity = 1.0
-                    }
-                }, label: {
-                    Image(systemName: "arrow.down.circle.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 40)
-                        .tint(.black)
-                })
-                
-                Text(currentTrivia.answer)
-                    .font(.title)
-                    .multilineTextAlignment(.center)
-                    .opacity(answerOpacity)
-                
+                if let currentTrivia = currentTrivia {
+                    
+                    Text(currentTrivia.question)
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                    
+                    Button(action: {
+                        withAnimation(.easeIn(duration: 1.0)) {
+                            answerOpacity = 1.0
+                        }
+                    }, label: {
+                        Image(systemName: "arrow.down.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 40)
+                            .tint(.black)
+                    })
+                    
+                    Text(currentTrivia.answer)
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                        .opacity(answerOpacity)
+                    
+                } else {
+                    ProgressView()
+                }
             }
             .navigationTitle("Trivia")
+        }
+        .task {
+            currentTrivia = await NetworkService.fetch()
         }
     }
 }
